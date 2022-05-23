78f71df9d9836316dbad6bc68f3ee3f6caabd2c0
Saif Ul Islam <saifulislam84210@gmail.com>
2022-05-23 14:15:22 +0500

    :wrench: feat: Added configuration for key
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

--- src/pupiilkey/key.py
+++ src/pupiilkey/key.py
@@ -3,21 +3,27 @@
 import socket
 from cryptography.fernet import Fernet
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 40008  # Port to listen on (non-privileged ports are > 1023)
+HOST = "127.52.0.2"  # Standard loopback interface address (localhost)
+PORT = 6000  # Port to listen on (non-privileged ports are > 1023)
 fernet: Fernet = Fernet(Fernet.generate_key())
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
 
-    while True:
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
+        print(f"[KEY::KEY] Starting connection to {HOST}, {PORT}")        
 
-                conn.sendall(fernet._signing_key + b"|" + fernet._encryption_key)
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print(f"[KEY] Connected by {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+
+                    conn.sendall(fernet._signing_key + b"|" + fernet._encryption_key)
+
+if __name__ == '__main__':
+    main()
